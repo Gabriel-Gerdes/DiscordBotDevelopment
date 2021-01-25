5f822a68d3e570043429e4869fe075ca099430e1
Gabriel-Gerdes <39770740+Gabriel-Gerdes@users.noreply.github.com>
2021-01-25 16:58:41 -0500

    Initial Commit

---
+++ AcademicCalendar/Discord.BotDevelopment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Discord.BotDevelopment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
