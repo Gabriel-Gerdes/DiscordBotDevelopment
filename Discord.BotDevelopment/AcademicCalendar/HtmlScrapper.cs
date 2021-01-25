050f87666780b24a16946da56c13ac3e720b9755
Gabriel-Gerdes <39770740+Gabriel-Gerdes@users.noreply.github.com>
2021-01-25 17:11:05 -0500

    Porting the academic calender from the PlayHouse

---
+++ Discord.BotDevelopment/AcademicCalendar/HtmlScrapper.cs
@@ -0,0 +1,60 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicCalendar
+{
+    public class HtmlScrapper
+    {
+
+        public List<List<string>> GetAcademicCalender()
+        {
+            string url = "https://www.odu.edu/academics/calendar/spring";
+            var response = CallUrl(url).Result;
+            var calendarEvents = ParseHtml(response);
+            return calendarEvents;
+        }
+        private static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
+            client.DefaultRequestHeaders.Accept.Clear();
+            var response = client.GetStringAsync(fullUrl);
+            return await response;
+        }
+        private List<List<string>> ParseHtml(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var calendarDate = htmlDoc.DocumentNode.Descendants("tr");
+
+            List<string> calendarDates = new List<string>();
+            foreach (var date in calendarDate)
+            {
+                if (date.HasChildNodes)
+                {
+                    String temp = date.InnerText.Replace("\n", " - ").Trim().Trim('-');
+                    calendarDates.Add(temp);
+                }
+            }
+
+            var listOfCalenders = new List<List<string>>();
+            if (calendarDates.Count > 30)
+            {
+
+                for (int i = 0; i < calendarDates.Count; i += 10)
+                {
+                    listOfCalenders.Add(calendarDates.GetRange(i, Math.Min(10, calendarDates.Count - i)));
+                }
+            }
+
+            return listOfCalenders;
+        }
+    }
+}
---
+++ Discord.BotDevelopment/AcademicCalendar/Program.cs
@@ -0,0 +1,108 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademicCalendar
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.MessageReceived += CommandHandler;
+
+            _client.Log += Log;
+
+            //Move this to an encrypted file
+            var token = File.ReadAllText("Token.txt");
+
+
+            await _client.LoginAsync(Discord.TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+        private Task CommandHandler(SocketMessage message)
+        {
+            string command = "";
+            int lengthOfCommand = -1;
+
+            //Filter non Prefix
+            if (!message.Content.StartsWith('!'))
+                return Task.CompletedTask;
+
+            //Removes messages from bots.
+            if (message.Author.IsBot)
+                return Task.CompletedTask;
+
+            if (message.Content.Contains(' '))
+                lengthOfCommand = message.Content.IndexOf(' ');
+            else
+                lengthOfCommand = message.Content.Length;
+
+            command = message.Content.Substring(1, lengthOfCommand - 1).ToLower();
+
+            //Actual Command processing
+            ExecuteCommand(command, message);
+
+            return Task.CompletedTask;
+        }
+        private Task ExecuteCommand(string Command, SocketMessage message)
+        {
+            try
+            {
+                switch (Command)
+                {
+                    case "hello":
+                        message.Channel.SendMessageAsync($"Hello {message.Author.Mention}");
+                        break;
+                    case "calendar":
+                        CalendarCommand(message);
+                        break;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return Task.CompletedTask;
+        }
+        private void CalendarCommand(SocketMessage message)
+        {
+            HtmlScrapper scrapper = new HtmlScrapper();
+            var calender = scrapper.GetAcademicCalender();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var supCalender in calender)
+            {
+                foreach (var eventDate in supCalender)
+                {
+                    sb.AppendLine(eventDate);
+                }
+                Console.WriteLine($"Sending {supCalender.Count} dates.");
+                message.Channel.SendMessageAsync(sb.ToString());
+                sb.Clear();
+            }
+        }
+    }
+}
+
